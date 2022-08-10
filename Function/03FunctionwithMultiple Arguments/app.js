68acd726ff0504739d71d3eba2ff00a01562615c
Upama <upamadolma@gmail.com>
2022-08-10 23:35:38 +0545

    return keyword

--- Function/03FunctionwithMultiple Arguments/app.js
+++ Function/03FunctionwithMultiple Arguments/app.js
@@ -3,18 +3,22 @@ function greet (firstName, lastName) {
     console.log(`Hi there, ${firstName} ${lastName[0]}.`) //0 represent the index of first letter of last name and choose that letter only 
 }
 
+//result firstname = upama lastname = nirdev it gives Hi there, upama n since 0 select only first letter for lastname 
+
 
 
 
 
 //function to repeat something 
 function repeat (str, numTimes){
-    let result = '';
-    for (i = 0; i < numTimes; i++) {
-        result += str
+    let result = '';  // result is empty so it can be used to all string till numtimes
+    for (i = 0; i < numTimes; i++) {  //numTimes give length
+        result += str  //result is empty but str is added till condition is true
     }
     console.log(result)
 }
+
+//result str= % numTimes = 5 it gives %%%%%
     
 
 
---
+++ Function/04Return Keyword/app.js
@@ -0,0 +1,9 @@
+
+function add (x , y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return false;   
+    }
+    return x + y;  //console.log(x+y) gives undefined 
+}
+
+//there are two return but return run only one time / it can return only one value 
