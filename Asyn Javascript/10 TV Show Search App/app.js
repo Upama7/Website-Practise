46f4bd214301497e04914660fd2f75d2dcba97a3
Upama <upamadolma@gmail.com>
2022-08-31 19:47:29 +0545

    tv show app

--- Asyn Javascript/10 TV Show Search App/app.js
+++ Asyn Javascript/10 TV Show Search App/app.js
@@ -2,9 +2,30 @@
 
 const form = document.querySelector("#formSearch")
 
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', async function (e) {
     e.preventDefault();
     // console.dir(form)  // help to see all attributes of form
-    const searchText = form.elements.query.value;
+    const searchTerm = form.elements.query.value;
     // console.log(form.elements.query.value) // gives us the value of what user types 
+    const config = {params: {q: searchTerm} }
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    // console.log(res.data)  //give directory of above url
+    // console.log(res.data[0].show);  //gives 1st data show info
+    makeImages(res.data)
+   form.elements.query.value = '';
 }) 
+
+
+
+
+//new function to create images from api https://api.tvmaze.com/singlesearch/shows?q=girls and we call this function in above form
+const makeImages = (shows) => {
+    for (let result of shows) {   //result represent each show in shows
+        // console.log(shows)   //gives info of all show
+        if (result.show.image) {
+            const img = document.createElement("img");
+            img.src = result.show.image.medium;
+            document.body.append(img)
+        }
+    }       
+}
